bd0bd4cba4089ee2034f3b20c58c1567a6514068
SamucaSilva00 <samuel.rodrigueslsilva@gmail.com>
2024-06-14 10:44:16 -0300

    Primeiras rotas do BackEnd

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { sequelize } from "./database";
 import { adminJsRouter, adminjs } from "./adminjs";
+import { router } from "./routes";
 
 const app = express()
 
 app.use(express.static('public'))
 app.use(adminjs.options.rootPath, adminJsRouter)
+app.use(router)
 
 const PORT = process.env.PORT || 3000
 
