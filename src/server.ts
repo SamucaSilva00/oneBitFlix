595186a9e94f58325680c8a500a431584f4d9f0e
SamucaSilva00 <samuel.rodrigueslsilva@gmail.com>
2024-06-20 10:16:08 -0300

    Registro de Usuários

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { router } from "./routes";
 const app = express()
 
 app.use(express.static('public'))
+app.use(express.json())
 app.use(adminjs.options.rootPath, adminJsRouter)
 app.use(router)
 
