563e8d208d77c09223671b6594a60e3039abe0e6
SamucaSilva00 <samuel.rodrigueslsilva@gmail.com>
2024-06-27 07:19:50 -0300

    Rotas protegidas usando JWT

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,8 @@ import { sequelize } from '../database'
 import { DataTypes, Model, Optional } from 'sequelize'
 import  bcrypt  from 'bcrypt'
 
+type checkPasswordCallback = (err?: Error | undefined, isSame?: boolean) =>  void
+
 export interface User {
   id: number
   firstName: string
@@ -15,7 +17,10 @@ export interface User {
 
 export interface UserCreationAttributes extends Optional<User, 'id'> {}
 
-export interface UserInstance extends Model<User, UserCreationAttributes>, User {}
+export interface UserInstance extends Model<User, UserCreationAttributes>, User {
+  checkPassword(password: any, arg1: (err: any, isSame: any) => import("express").Response<any, Record<string, any>>): unknown
+  checkpassword: (password: string, callbackfn: checkPasswordCallback) => void
+}
 
 export const User = sequelize.define<UserInstance, User>('users', {
   id: {
@@ -68,3 +73,13 @@ export const User = sequelize.define<UserInstance, User>('users', {
         }
     }
 })
+
+User.prototype.checkPassword = function (password: string, callbackfn: (err: Error | undefined, isSame: boolean) => void) {
+  bcrypt.compare(password, this.password, (err, isSame) => {
+    if (err) {
+      callbackfn(err, false)
+    } else {
+      callbackfn(err, isSame)
+    }
+  })
+}
